b199ad03124af016d74ac8edb7e6fbdbe5d46018
Luis Steidle <luis@steidle.me>
2022-02-12 19:03:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# basetab
+
+A template for a responsive browser startpage.
+
+Based on [jet](https://github.com/marcamos/jet)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.html"],
+  },
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Clash Display", "sans-serif"],
+        body: ["Overpass Mono", "monospace"],
+      },
+      colors: {
+        "hsl-blue": ["hsl(243, 80%, 62%)"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
